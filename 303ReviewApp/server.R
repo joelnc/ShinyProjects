77a1e1d301a636dfa938951077a8a96d4857e627
joelnc <joel.nipper@charlottenc.gov>
2017-02-03 16:18:33 -0500

    Added Background Docs Tab, updated text for 303ReviewApp
    Also, changed a few of the default analytes (i.e. ~=Silver)

--- 303ReviewApp/server.R
+++ 303ReviewApp/server.R
@@ -214,12 +214,14 @@ shinyServer(
                             list(fillcolor = "green",
                                 line = list(color = "white"),
                                 opacity = 0.3, type = "rect",
-                                x0 = "2010-01-01", x1 = "2017-02-01",
+                                x0=input$dates[1], x1=input$dates[2],
+                                ##x0 = "2010-01-01", x1 = "2017-02-01",
                                 xref = "x", y0 = -yMax, y1 = 0, yref = "y"),
                             list(fillcolor = "red",
                                 line = list(color = "white"),
                                 opacity = .20, type = "rect",
-                                x0 = "2010-01-01", x1 = "2017-02-01",
+                                x0=input$dates[1], x1=input$dates[2],
+                                ##x0 = "2010-01-01", x1 = "2017-02-01",
                                 xref = "x", y0 = 0, y1 = yMax, yref = "y")
                         ),
                         showlegend=TRUE
@@ -284,12 +286,14 @@ shinyServer(
                             list(fillcolor = "green",
                                 line = list(color = "white"),
                                 opacity = 0.3, type = "rect",
-                                x0 = "2010-01-01", x1 = "2017-02-01",
+                                x0=input$dates[1], x1=input$dates[2],
+                                ##x0 = "2010-01-01", x1 = "2017-02-01",
                                 xref = "x", y0 = -yMax, y1 = 0, yref = "y"),
                             list(fillcolor = "red",
                                 line = list(color = "white"),
                                 opacity = .20, type = "rect",
-                                x0 = "2010-01-01", x1 = "2017-02-01",
+                                x0=input$dates[1], x1=input$dates[2],
+                                ##x0 = "2010-01-01", x1 = "2017-02-01",
                                 xref = "x", y0 = 0, y1 = yMax, yref = "y")
                         )
                     )
@@ -389,7 +393,6 @@ shinyServer(
 
 
         })
-
 
     }) ## Done
 
