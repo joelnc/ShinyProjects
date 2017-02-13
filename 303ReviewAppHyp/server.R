84b034fd8c1fe188e98fa3c79fca50c7020ef66a
joelnc <joel.nipper@charlottenc.gov>
2017-02-13 17:02:49 -0500

    Added Toggle Function to Public Side App

---
+++ 303ReviewAppHyp/server.R
@@ -0,0 +1,169 @@
+library(shiny)
+
+## Define server logic reqd to draw hist
+shinyServer(
+    function(input, output) {
+
+#############################################################################
+######################### Hypothetical Data Functions #######################
+#############################################################################
+
+        ## Set test data
+        data1 <- c(11,6,2,11,3,7,3,4,12,4,3,5,1)
+        data2 <- c(9,8,85,8,9,74,9,8,92,7,8,9,8)
+        data3 <- c(9,6,2,11,3,7,3,4,12,4,3,5,1)
+        data4 <- c(9,6,2,11,3,7,3,4,6,4,3,5,1)
+        data5 <- c(9,6,2,4,3,7,3,4,6,4,3,5,1)
+        data6 <- c(rep(c(4,3,7,1,5,4),3), 4,3,7,1,5)
+        data7 <- c(rep(c(4,3,7,1,5,4),3), 12, rep(c(4,3,7,1,5,4),3))
+        data8 <- c(rep(c(4,3,7,1,5,4),3), 12,4,3,7,14, rep(c(4,3,7,1,5,4),5))
+        dataList <- list(data1=data1, data2=data2, data3=data3, data4=data4,
+                         data5=data5, data6=data6, data7=data7, data8=data8)
+
+        ## Reactive plot one of TS concentrations
+        output$tsPlot <- renderPlot({
+
+            ## Data time series- use the pass in input$dataset to select column
+            par(xaxs="i", yaxs="i", mar=c(5,8,2,2),
+                fig=c(0,.75,0,1))
+            dt <- seq.Date(from=as.Date("2015-01-01"), by="month",
+                           length.out=length(dataList[[input$dataset]]))
+            plot(dt, dataList[[input$dataset]], ylim=c(0,100), cex=1.2,
+                 pch=18, xlab=NA, ylab=NA)
+            lines(x=dt,y=rep(10,length(dt)), col="red")
+            mtext(2, text="Conc. (mg/L)", cex=1.4, font=2,
+                  line=3)
+
+            par(xaxs="i", yaxs="i", mar=c(5,4 ,2,2),
+                fig=c(0.75,1,0,1), new=TRUE)
+
+            hData <- data.frame(exc=0, notExc=0)
+            hData$exc <- hData$exc+table(dataList[[input$dataset]]>10)[2]
+            hData$notExc <- hData$notExc+table(dataList[[input$dataset]]>10)[1]
+
+            barplot(c(hData[1,1], hData[1,2]), space=0, axes=FALSE,
+                    col="blue",
+                    ylim=c(0, length(dataList[[input$dataset]]+2)))
+            box()
+            axis(1,at=c(0.5, 1.5), labels=c("Yes", "No"))
+            axis(2,at=seq(0,55,5))
+            mtext(side=1, text="Exceeds Standard?", line=3,font=2, cex=1.25)
+
+        })
+
+        ## Plot fun
+        excPlotFun <- function(biTest, alt) {
+            plot(x=NA, y=NA, xlim=c(0,1), ylim=c(0,1),
+                 pch=NA, xlab=NA, ylab=NA, axes=FALSE)
+            if (alt=="g"){
+                rect(xleft=0, xright=0.1, ybottom=0, ytop=1,
+                     col="grey90", border=NA)
+            } else if (alt=="l") {
+                rect(xleft=0.10, xright=1, ybottom=0, ytop=1,
+                     col="grey90", border=NA)
+            }
+            points(x=biTest$estimate, y=0.5, pch=18, cex=2)
+            lines(x=rep(0.10,2), y=c(0,1), col="red",lty=2)
+            lines(x=rep(biTest$conf.int[1],2), y=c(0.4, 0.6), lwd=2)
+            lines(x=rep(biTest$conf.int[2],2), y=c(0.4, 0.6), lwd=2)
+            lines(x=c(biTest$conf.int[1], biTest$conf.int[2]),
+                  y=c(0.5, 0.5), lwd=2)
+            box()
+            axis(1, at=seq(0,1,0.1), labels=seq(0,100,10))
+            mtext(side=1, text="Exceedance Percent (%)",
+                  line=3, font=2, cex=1.25)
+        }
+
+        ## ... Repeat yourself
+        output$excPlotG <- renderPlot({
+            ## Do the binomial tests (maybe move to above)
+            useTestG <- binom.test(x=sum(dataList[[input$dataset]]>=10),
+                                  n=length(dataList[[input$dataset]]), p=0.10,
+                                  alternative="g", conf.level=0.90)
+
+            ## Plot function for binomial plots
+            par(xaxs="i", yaxs="i", fig=c(0,1,0.5,1))
+
+            ## Call it, 2x
+            excPlotFun(biTest=useTestG, alt="g")
+        })
+
+        ## ... Repeat yourself
+        output$excPlotL <- renderPlot({
+            ## Do the binomial tests (maybe move to above)
+            useTestL <- binom.test(x=sum(dataList[[input$dataset]]>=10),
+                                  n=length(dataList[[input$dataset]]), p=0.10,
+                                  alternative="l", conf.level=0.90)
+
+            ## Plot function for binomial plots
+            par(xaxs="i", yaxs="i", fig=c(0,1,0.5,1))
+
+            ## Call it, 2x
+            excPlotFun(biTest=useTestL, alt="l")
+        })
+
+        ## ... Repeat yourself
+        output$excPlotT <- renderPlot({
+            ## Do the binomial tests (maybe move to above)
+            useTestT <- binom.test(x=sum(dataList[[input$dataset]]>=10),
+                                  n=length(dataList[[input$dataset]]), p=0.10,
+                                  alternative="t", conf.level=0.90)
+
+            ## Plot function for binomial plots
+            par(xaxs="i", yaxs="i", fig=c(0,1,0.5,1))
+
+            ## Call it, 2x
+            excPlotFun(biTest=useTestT, alt="t")
+        })
+
+
+        ## Function to plot the 90% confidence (toggleable) fig
+        excPlotFunTog <- function(biTest, alt) {
+            plot(x=NA, y=NA, xlim=c(0,1), ylim=c(0,1),
+                 pch=NA, xlab=NA, ylab=NA, axes=FALSE)
+            if (alt=="g"){
+                rect(xleft=0, xright=input$cutoff, ybottom=0, ytop=1,
+                     col="grey90", border=NA)
+            } else if (alt=="l") {
+                rect(xleft=input$cutoff, xright=1, ybottom=0, ytop=1,
+                     col="grey90", border=NA)
+            }
+            points(x=biTest$estimate, y=0.5, pch=18, cex=2)
+            lines(x=rep(input$cutoff,2), y=c(0,1), col="red",lty=2)
+            lines(x=rep(biTest$conf.int[1],2), y=c(0.4, 0.6), lwd=2)
+            lines(x=rep(biTest$conf.int[2],2), y=c(0.4, 0.6), lwd=2)
+            lines(x=c(biTest$conf.int[1], biTest$conf.int[2]),
+                  y=c(0.5, 0.5), lwd=2)
+            box()
+            axis(1, at=seq(0,1,0.1), labels=seq(0,100,10),
+                 cex.axis=1.25)
+            mtext(side=1, text="Exceedance Percent (%)",
+                  line=3, font=2, cex=1.5)
+        }
+
+        ## ... Repeat yourself
+        output$excPlotTog <- renderPlot({
+##browser()
+            ## Get metal data, hardness, standard at that hardness
+            useData <- dataList[[input$dataset]]
+                ## returnDiffData(site=input$sitecode,
+                ##                      metal=input$analyte,
+                ##                         dates=input$dates)
+
+            binomData <- useData-10
+
+            useTest <- binom.test(x=sum(binomData>=0),
+                                   n=length(binomData),
+                                   p=input$cutoff,
+                                  alternative=input$radio, conf.level=input$conf)
+
+            ## Plot function for binomial plots
+            par(xaxs="i", yaxs="i", fig=c(0,1,0.5,1))
+
+            ## Call it, 2x
+            excPlotFunTog(biTest=useTest, alt=input$radio)
+        })
+
+
+    }) ## Done
+
---
+++ 303ReviewAppHyp/ui.R
@@ -0,0 +1,136 @@
+library(shiny)
+source("metalsFun.r")
+source("metalsThresFuns.r")
+
+f <- "appText.dat"
+con <- file(f, open="r")
+t <- readLines(con)
+t <- t[which(t!="")]
+close(con)
+textI <- 1
+
+## Load, format, subset, WQD File
+## wqDF <- read.csv("LabReport.txt",
+##                  stringsAsFactors=FALSE, sep=",", header=TRUE)
+## wqDF$Coldate <- as.POSIXct(wqDF$Coldate, format="%m/%d/%Y %H:%M:%S")
+## wqDF$Date <- as.Date(wqDF$Coldate)  ## If needed
+## wqDF <- wqDF[which(wqDF$Analyte %in%
+##                    c("Copper", "Lead", "Zinc", "Cadmium", "Chromium",
+##                      "Beryllium", "Selenium", "Arscenic", "Silver")), ]
+## wqDF <<- wqDF[order(wqDF$Coldate), ]
+
+## Define UI for applicaiton that draws a hist
+shinyUI(
+    navbarPage(title="Exceedance Demos",
+               selected="Hypothetical",
+               br(),
+               p(t[1]),
+               fluidRow(
+                   column(12, h3("Hypothetical Data Selection", align="center"),
+                          wellPanel(
+                              selectInput(
+                                  inputId= "dataset",
+                                  label="Select Dataset: ",
+                                  choices=c("Slight (3 exceedances out of 13)"="data1",
+                                            "Vast (3 exceedances out of 13)"="data2",
+                                            "2 exceedances out of 13"="data3",
+                                            "1 exceedance out of 13"="data4",
+                                            "0 exceedances out of 13"="data5",
+                                            "0 exceedances out of 23"="data6",
+                                            "1 exceedance out of 37"="data7",
+                                            "2 exceedances out of 52"="data8"),
+                                  selected="data1")
+                          )
+                          )
+               ),
+               tabPanel("Hypothetical",
+                        tabsetPanel(type="tabs",
+                                    tabPanel(title="Time Series",
+                                             h4("Hypothetical Time Series"),
+                                             t[2], br(), br(), t[3], plotOutput("tsPlot")),
+                                    tabPanel(title="Listing Analysis",
+                                             h4("Hypothetical Time Series"),
+                                             t[4],
+                                             plotOutput("excPlotG")),
+                                    tabPanel(title="Delisting Analysis*",
+                                             h4("Hypothetical Time Series"),
+                                             t[5], br(), br(), t[6],
+                                             fluidRow(
+                                                 column(12, align="center",
+                                                        plotOutput("excPlotL")
+                                                        )
+                                                 )
+                                             ),
+                                    tabPanel(title="Two Sided Test",
+                                             h4("Hypothetical Time Series"),
+                                             t[7], br(), br(), t[8],
+                                             plotOutput("excPlotT")),
+                                    tabPanel(title="Toggleable",
+                                             fluidRow(
+                                                 column(5,
+                                                        radioButtons(inputId="radio",
+                                                                     label = h3("Hypothesis Tests"),
+                                                                     choices = list(
+                                                                         "Listing (assuming the site is not 303(d) listed for this pollutant)" = "g",
+                                                                         "Delisting (assuming the site is 303(d) listed for this pollutant)" = "l",
+                                                                         "Two-sided!"= "t"),
+                                                                     selected = "g")
+                                                        ),
+                                                 column(7,
+                                                        br(),
+                                                        sliderInput(inputId="conf",
+                                                                    label="Condidence Level (90% = 0.9)",
+                                                                    min=0, max=1,
+                                                                    value=.9, step=0.01,
+                                                                    round=FALSE,
+                                                                    animate=TRUE),
+                                                        sliderInput(inputId="cutoff",
+                                                                    label="Exceedance Level (10% = 0.10)",
+                                                                    min=0, max=1,
+                                                                    value=.1, step=0.01,
+                                                                    round=FALSE,
+                                                                    animate=TRUE)
+                                                        )
+                                             ),
+                                             textOutput("confText"),
+                                             fluidRow(
+                                                 column(9,
+                                                        plotOutput("excPlotTog", height="500px")
+                                                        ),
+                                                 column(3,
+                                                        br(), br(),
+                                                        img(src="stormy.jpg",
+                                                            height="200", width="125")
+                                                        )
+                                             )
+                                    ),
+                                    tabPanel("'One in Three'",
+                                             br(), t[9]),
+                                    tabPanel("Backgroud Docs.",
+                                              br(),
+                                              h3("Related Documents"),
+                                              a("EPA Public Notice Decision / 303(d) Listing Letter",
+                                                href="https://www.epa.gov/sites/production/files/2016-12/documents/nc2016_303ddecisionpackage20161208_reduced.pdf"),
+                                              br(), br(),
+                                              a("Lin et al. 2000",
+                                                href="http://infohouse.p2ric.org/ref/41/40276.pdf")
+                                              )
+                                    )
+                        ),
+               tabPanel("CharMeck Metals",
+                        tabsetPanel(type="tabs",
+                                    tabPanel(title="Time Series Conc."
+                                             ),
+                                    tabPanel(title="Time Series Exceedances"
+                                             ),
+                                    tabPanel(title="90% Confidence"
+                                             )
+                                    )
+                        )
+               )
+)
+
+
+
+
+
